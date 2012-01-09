a1e9028570713248ee9c1b5c398bf896eefda461
Valentin Hilbig <webmaster@scylla-charybdis.com>
2012-01-09 15:00:39 +0100

    Added workaround to add GIT PS1 on Non-Debian-OS

--- fix-bashrc.sh
+++ fix-bashrc.sh
@@ -11,6 +11,11 @@ fgrep -qx "$1" "$TMP" || echo "$1" >> "$TMP"
 # Inserts the `$(__git_ps1 "(%s)")` right before the `$` on the PS1 prompt
 sed -e '/__git_ps1/b' -e 's|\(^[[:space:]]*PS1='\''.*\)\(\\\$ '\''[[:space:]]*\)$|\1\$(__git_ps1 "(%s)")\2|' "$RC" > "$TMP"
 
+grep -w PS1 "$TMP" ||
+cat <<'EOF' >> "$TMP"
+[ -z "$PS1" ] || PS1="`echo "$PS1" | sed -e '/__git_ps1/b' -e 's|\(\\\\\\\$[[:space:]]*\)$|\$(__git_ps1 "(%s)")\1|'`"
+EOF
+
 # Append some proper settings for __git_ps1
 add "export GIT_PS1_SHOWDIRTYSTATE=yes"
 add "export GIT_PS1_SHOWUNTRACKEDFILES=yes"
