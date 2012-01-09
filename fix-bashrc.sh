b9d3c318a2b7445d9c80bbfa49b9e97a756b8a6a
Valentin Hilbig <webmaster@scylla-charybdis.com>
2012-01-09 15:23:43 +0100

    Quiesce the grep, looks funny else

--- fix-bashrc.sh
+++ fix-bashrc.sh
@@ -11,7 +11,7 @@ fgrep -qx "$1" "$TMP" || echo "$1" >> "$TMP"
 # Inserts the `$(__git_ps1 "(%s)")` right before the `$` on the PS1 prompt
 sed -e '/__git_ps1/b' -e 's|\(^[[:space:]]*PS1='\''.*\)\(\\\$ '\''[[:space:]]*\)$|\1\$(__git_ps1 "(%s)")\2|' "$RC" > "$TMP"
 
-grep -w PS1 "$TMP" ||
+grep -qw PS1 "$TMP" ||
 cat <<'EOF' >> "$TMP"
 [ -z "$PS1" ] || { __i=; for __a in /usr/share/doc/git-*/contrib/completion/git-completion.bash; do [ -r "$__a" ] && __i="$__a"; done; [ -n "$__i" ] && . "$__i" && PS1="`echo "$PS1" | sed -e '/__git_ps1/b' -e 's|\(\\\\\\\$[[:space:]]*\)$|\$(__git_ps1 "(%s)")\1|'`"; unset __a __i; }
 EOF
