ac4b2706134852e1d21b45db7891c760baf151dc
Valentin Hilbig <webmaster@scylla-charybdis.com>
2012-09-08 22:47:38 +0200

    gitstart-ls.sh limits output to values on commandline to aid copy+paste

--- gitstart-ls.sh
+++ gitstart-ls.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+list()
+{
 for a in "$HOME/.ssh"/*.pub
 do
 	ssh-keygen -l -f "${a%.pub}"
 done
+}
+
+if [ 0 = "$#" ]
+then
+	list
+else
+	for a
+	do
+		list | fgrep -- "$a"
+	done
+fi
