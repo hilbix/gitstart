11c18770b39fd7e028fbb40daf99f6ed8815eaec
Valentin Hilbig <webmaster@scylla-charybdis.com>
2015-06-13 19:38:37 +0200

    alias colored less, ll la

--- aliases.sh
+++ aliases.sh
@@ -17,8 +17,12 @@ a bvv	branch -avv
 a bv	"!git branch -avv | sed 's/^/x/' | awk '\$3!=\"->\" { m[\$3]=m[\$3] substr(\$1,2); sub(/^remotes\\//,\"/\",\$2); if (!f[\$3]) { f[\$3]=\$2; if (length(\$2)>mx) mx=length(\$2); } else { l=length(\$2)-length(f[\$3]); if (substr(\$2,l)==\"/\"f[\$3]) \$2=substr(\$2,1,l); k[\$3]=k[\$3] \" \" \$2; } } END { for (a in f) printf(\"x%-1s %s %-*s %s\\n\", m[a], a, mx, f[a], k[a]); }' | sort -bk2 | sed 's/^x//'"
 a check	diff --check
 a co	checkout
-a ls	log --graph --oneline
-a st	'!f() { cd "$GIT_PREFIX" && git status "$@" | less -XF; }; f'
+a pager	'!pager() { cd "$GIT_PREFIX" && git -c color.status=always -c color.ui=always "$@" 2>&1 | less -XFR; }; pager'
+a pageat '!pager() { at="$1" && shift && cd "$GIT_PREFIX" && git -c color.status=always -c color.ui=always "$@"  2>&1 | less -XFRp "$at"; }; pager'
+a ls	'!cd "$GIT_PREFIX" && git pager log --color=always --graph --oneline --decorate'
+a ll	'!cd "$GIT_PREFIX" && git pager log --color=always --graph --oneline --decorate --numstat'
+a la	'!cd "$GIT_PREFIX" && git pager log --color=always --graph -u --decorate'
+a st	'!cd "$GIT_PREFIX" && git pager status'
 a up	status
 a squash rebase --interactive
 #a fastforward # git fetch; git fastforward -> ff all branches which can do so, flag which cannot
@@ -40,8 +44,9 @@ a switch '!f() { git show-ref --heads --verify "refs/heads/$1" && git symbolic-r
 # Basic idea from https://gist.github.com/jehiah/1288596
 # f() trick from http://stackoverflow.com/questions/7005513/pass-an-argument-to-a-git-alias-command
 a relate '!f(){ x="$1"; case "$#" in 0|1) git for-each-ref --format="%(refname:short) %(upstream:short)" refs;; *) shift; for a in "$@"; do echo "$a"; done;; esac | while read -r l r; do printf "%24s %s\\n" "$(git rev-list --cherry-mark --dense --left-right --boundary --oneline "${x:-${r:-HEAD}}...$l" -- | sed "s/^\\(.\\).*/\\1/" | sort | uniq -c | tr -d " " | tr "\\n" " ")" "${x:-${r:-HEAD}}...$l"; done; }; f'
-a graph '!f(){ case "$#:$1" in 0:) r="HEAD...HEAD@{u}";; 1:*...*) r="$1";; 1:*) r="HEAD...$1";; *) r="$1...$2";; esac; git rev-list --cherry-mark --dense --left-right --boundary --pretty --graph "$r" -- | less -XFp "$(git rev-parse "${r%...*}")"; }; f'
-a graph1 '!f(){ case "$#:$1" in 0:) r="HEAD...HEAD@{u}";; 1:*...*) r="$1";; 1:*) r="HEAD...$1";; *) r="$1...$2";; esac; git rev-list --cherry-mark --dense --left-right --boundary --oneline --graph "$r" -- | less -XFp " $(git rev-parse --short "${r%...*}") "; }; f'
+a dograph '!graph(){ case "$#:$3" in 2:) r="HEAD...HEAD@{u}";; 3:*...*) r="$3";; 3:*) r="HEAD...$3";; *) r="$3...$4";; esac; r1="$(git rev-parse "${r%%...*}")"; r2="$(git rev-parse "${r##*...}")"; echo "$r - $r1 - $r2"; r1s=" $(git rev-parse --short "$r1") "; eval "v=\"\$$1\""; if [ ".$r1" = ".$r2" ]; then git pageat "${v# }" log --color=always $2 -1 "$r1"; else git pageat "$v" rev-list --color=always --cherry-mark --dense --left-right --boundary $2 --graph "$r1...$r2" --; fi; }; graph'
+a graph '!git dograph r1 --pretty'
+a graph1 '!git dograph r1s --oneline'
  
 # This is "git cherry" with something like an UI
 a carry	!git-carry.sh
