c43aaf12e95bac03f5e2562063aa18f0b5bdaea2
Valentin Hilbig <webmaster@scylla-charybdis.com>
2017-11-11 12:28:02 +0100

    alias git submodules-register

--- aliases.sh
+++ aliases.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-export LC_ALL=C
+export LC_ALL=C.UTF-8 || export LC_ALL=C
 
 s() { git config --global --replace-all "$1" "${*:2}"; }
 a() { s "alias.$@"; }
+# bash wrapper
+b() { a "$1" "!LC_ALL=$LC_ALL bash -c '$(sed "s/'/'\\\\''/g" | tr '\n' '\t' | sed 's/\t\t*/ /g')' --"; }
 
 a alias	!git-alias.sh
 a amend	commit --amend
 a amit	commit --amend -C HEAD
 a bvv	branch -avv
-a bv	"!LC_ALL=C git branch -avv | sed 's/^/x/' | awk '\$2==\"(detached\" && \$3==\"from\" && \$4==\$5\")\" { \$2=\"HEAD\"; \$3=\$5; } \$2==\"(HEAD\" && \$3==\"detached\" && \$4==\"at\" && \$5==\$6\")\" { \$2=\"HEAD\"; \$3=\$6; } \$3!=\"->\" { m[\$3]=m[\$3] substr(\$1,2); sub(/^remotes\\//,\"/\",\$2); if (!f[\$3]) { f[\$3]=\$2; if (length(\$2)>mx) mx=length(\$2); } else { l=length(\$2)-length(f[\$3]); if (substr(\$2,l)==\"/\"f[\$3]) \$2=substr(\$2,1,l); k[\$3]=k[\$3] \" \" \$2; } } END { for (a in f) printf(\"x%-1s %s %-*s %s\\n\", m[a], a, mx, f[a], k[a]); }' | sort -bk2 | sed 's/^x//'"
+b bv	<<'EOF'
+git branch -avv |
+sed 's/^/x/' |
+awk '
+$2=="(detached" && $3=="from" && $4==$5")"		{ $2="HEAD"; $3=$5; }
+$2=="(HEAD" && $3=="detached" && $4=="at" && $5==$6")"	{ $2="HEAD"; $3=$6; }
+$3!="->"	{
+		m[$3]=m[$3] substr($1,2); sub(/^remotes\//,"/",$2);
+		if (!f[$3])
+			{
+			f[$3]=$2;
+			if (length($2)>mx) mx=length($2);
+			}
+		else	{
+			l=length($2)-length(f[$3]);
+			if (substr($2,l)=="/"f[$3]) $2=substr($2,1,l);
+			k[$3]=k[$3] " " $2;
+			}
+		}
+END	{
+	for (a in f)
+		printf("x%-1s %s %-*s %s\n", m[a], a, mx, f[a], k[a]);
+	}' |
+sort -bk2 |
+sed 's/^x//'
+EOF
 # Same for tags
 a tv	"!{ git tag --format='%(objectname)	%(refname:strip=2)'; git remote | while read -r n; do echo -n \" \$n\" >&2; git ls-remote --tags \"\$n\" | awk -F'\\t' -vN=\"\$n\" '{ sub(/^[^/]*\/[^/]*/,\"\",\$2); print \$1 \"\\t/\" N \$2 }'; done; echo >&2; } | sort -r | awk '{ if (!f[\$1]) { f[\$1]=\$2; if (mx<length(\$2)) mx=length(\$2); } else { l=length(\$2)-length(f[\$1]); if (\"/\"f[\$1]!=substr(\$2,l)) l=length(\$2); m[\$1]=m[\$1] \" \" substr(\$2,0,l); } } END { for (a in f) printf(\"%s %-*s %s\\n\", a, mx, f[a], m[a]); }' | sort"
 a check	diff --check
@@ -30,10 +57,49 @@ a squash rebase --interactive
 a amend-tag	'!f(){ [ 1 = $# ] || { echo "amend-tag needs tag name"; return 1; }; git tag -f -a -- "$1" "$1^{}"; }; f'
 
 # See https://stackoverflow.com/a/44973360
-a sdiff	'!'"bash -c 'O=(); A=(); while x=\"\$1\"; shift; do case \$x in -*) O+=(\"\$x\");; *) A+=(\"\$x^{}\");; esac; done; g(){ git show \"\${A[\$1]}\" && return; echo FAIL \${A[\$1]}; git show \"\${A[\$2]}\"; }; diff \"\${O[@]}\" <(g 0 1) <(g 1 0)' --"
+b sdiff	<<'EOF'
+O=();
+A=();
+while	x="$1";
+	shift;
+do
+	case $x in
+	-*)	O+=("$x");;
+	*)	A+=("$x^{}");;
+	esac;
+done;
+g()
+{
+git show "${A[$1]}" && return;
+echo FAIL ${A[$1]};
+git show "${A[$2]}";
+};
+diff "${O[@]}" <(g 0 1) <(g 1 0)
+EOF
 a udiff	'!git sdiff -u'
 a bdiff	'!git sdiff -b'
 a ddiff	'!git pager udiff'
+
+# Register all submodules which are missing in the index
+# Why isn't there an option to `submodule init` for this?
+b submodules-register <<'EOF'
+git config -f .gitmodules -z --get-regexp '^submodule\..*\.path$' |
+sed -z 's/\n.$//' |
+tr '\0' '\n' |
+while read -r key;
+do
+	sub="${key%.path}";
+	pth="$(git config -f .gitmodules --get "$sub.path")" &&
+	url="$(git config -f .gitmodules --get "$sub.url")" &&
+	if	bra="$(git config -f .gitmodules --get "$sub.branch")" &&
+		[ -n "$bra" ];
+	then
+		git submodule add -b "$bra" -- "$url" "$pth";
+	else
+		git submodule add -- "$url" "$pth";
+	fi;
+done
+EOF
 
 # As suggested by Daniel Brockman, see http://stackoverflow.com/questions/957928/is-there-a-way-to-get-the-git-root-directory-in-one-command#comment9747528_957978
 a exec	'!exec '
